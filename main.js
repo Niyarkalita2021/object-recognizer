2827d1161a9a53048a18b25ff47121d731ab2ba0
Niyarkalita2021 <82107686+Niyarkalita2021@users.noreply.github.com>
2021-10-25 20:51:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 img = "";
+objects = [];
 status = "";
 
 function preload(){
-    img = loadImage('dog_cat.jpg')
+    img = loadImage('Object image.jpg');
 }
 function setup() {
     canvas = createCanvas(640, 420);
@@ -22,20 +23,24 @@ function gotResult(error, results) {
         console.log(error);
     }
       console.log(results);
+      objects = results;
 }
 
 
 function draw() {
     image(img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
+    
+    if(status != "")
+    {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detector";
 
-    fill("#FF0000");
-    text("Cat", 320, 120);
-    noFill();
-    stroke("#FF0000")
-    rect(300, 90, 270, 320);
+            fill("#0000FF");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#0000FF");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
